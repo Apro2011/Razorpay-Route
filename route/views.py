e0288399ea4b2032c0e97a51e31327b4df4b8ac1
Apro2011 <biswasaprotim2011@outlook.com>
2024-08-21 20:01:33 +0530

    API changes

--- route/views.py
+++ route/views.py
@@ -152,35 +152,45 @@ class RecieverList(APIView):
                 )
 
             # Create Linked Accounts
-            accounts_url = "https://api.razorpay.com/v2/accounts"
+            accounts_url = "https://api.razorpay.com/v1/beta/accounts"
             account_data = {
                 "email": serializer.validated_data.get("email"),
-                "phone": serializer.validated_data.get("phone"),
-                "type": "route",
-                "reference_id": serializer.validated_data.get("reference_id"),
-                "legal_business_name": serializer.validated_data.get(
-                    "legal_business_name"
-                ),
-                "business_type": "individual",
-                "contact_name": serializer.validated_data.get("contact_name"),
-                "profile": {
-                    "category": "healthcare",
-                    "subcategory": "clinic",
-                    "addresses": {
-                        "registered": {
-                            "street1": serializer.validated_data.get("street1"),
-                            "street2": serializer.validated_data.get("street2"),
-                            "city": serializer.validated_data.get("city"),
-                            "state": serializer.validated_data.get("state"),
-                            "postal_code": serializer.validated_data.get("postal_code"),
-                            "country": "IN",
-                        }
-                    },
+                "tnc_accepted": True,
+                # "phone": serializer.validated_data.get("phone"),
+                # "type": "route",
+                # "reference_id": serializer.validated_data.get("reference_id"),
+                "account_details": {
+                    "business_name": serializer.validated_data.get(
+                        "legal_business_name"
+                    ),
+                    "business_type": "individual",
                 },
-                "legal_info": {
-                    "pan": serializer.validated_data.get("pan"),
-                    "gst": serializer.validated_data.get("gst"),
+                "name": serializer.validated_data.get("contact_name"),
+                "bank_account": {
+                    "account_number": serializer.validated_data.get("account_number"),
+                    "ifsc_code": serializer.validated_data.get("ifsc_code"),
+                    "beneficiary_name": serializer.validated_data.get(
+                        "legal_business_name"
+                    ),
                 },
+                # "profile": {
+                #    "category": "healthcare",
+                #    "subcategory": "clinic",
+                #    "addresses": {
+                #        "registered": {
+                #            "street1": serializer.validated_data.get("street1"),
+                #            "street2": serializer.validated_data.get("street2"),
+                #            "city": serializer.validated_data.get("city"),
+                #            "state": serializer.validated_data.get("state"),
+                #            "postal_code": serializer.validated_data.get("postal_code"),
+                #            "country": "IN",
+                #        }
+                #    },
+                # },
+                # "legal_info": {
+                #    "pan": serializer.validated_data.get("pan"),
+                #    "gst": serializer.validated_data.get("gst"),
+                # },
             }
 
             account_response = requests.post(
@@ -214,130 +224,145 @@ class RecieverList(APIView):
                 razor_id = json.loads(account_response.content.decode("utf-8"))["id"]
                 serializer.validated_data["razor_id"] = razor_id
                 serializer.save()
-                stakeholder_url = (
-                    "https://api.razorpay.com/v2/accounts/" + razor_id + "/stakeholders"
+                return Response(
+                    {
+                        "data": {
+                            "database_data": serializer.data,
+                            "razorpay_data": [
+                                account_response.json(),
+                                # stakeholder_response.json(),
+                                # product_config_response.json(),
+                                # update_product_config_response.json(),
+                            ],
+                        },
+                        "status": True,
+                    },
+                    status=status.HTTP_201_CREATED,
                 )
+                # stakeholder_url = (
+                #    "https://api.razorpay.com/v2/accounts/" + razor_id + "/stakeholders"
+                # )
 
                 # Create Stakeholder account
-                stakeholder_data = {
-                    "name": serializer.validated_data["contact_name"],
-                    "email": serializer.validated_data["email"],
-                    "addresses": {
-                        "residential": {
-                            "street": serializer.validated_data["street1"],
-                            "city": serializer.validated_data["city"],
-                            "state": serializer.validated_data["state"],
-                            "postal_code": serializer.validated_data["postal_code"],
-                            "country": "IN",
-                        }
-                    },
-                }
+                # stakeholder_data = {
+                #    "name": serializer.validated_data["contact_name"],
+                #    "email": serializer.validated_data["email"],
+                #    "addresses": {
+                #        "residential": {
+                #            "street": serializer.validated_data["street1"],
+                #            "city": serializer.validated_data["city"],
+                #            "state": serializer.validated_data["state"],
+                #            "postal_code": serializer.validated_data["postal_code"],
+                #            "country": "in",
+                #        }
+                #    },
+                # }
 
-                stakeholder_response = requests.post(
-                    stakeholder_url,
-                    auth=(settings.RAZOR_KEY_ID, settings.RAZOR_KEY_SECRET),
-                    headers={"Content-Type": "application/json"},
-                    json=stakeholder_data,
-                )
+                # stakeholder_response = requests.post(
+                #    stakeholder_url,
+                #    auth=(settings.razor_key_ID, settings.RAZOR_KEY_SECRET),
+                #    headers={"content-type": "application/json"},
+                #    json=stakeholder_data,
+                # )
 
-                if stakeholder_response.status_code != 200:
-                    return Response(
-                        {
-                            "data": stakeholder_response.json(),
-                            "status": False,
-                        },
-                        status=status.HTTP_400_BAD_REQUEST,
-                    )
-                else:
-                    product_config_url = (
-                        "https://api.razorpay.com/v2/accounts/" + razor_id + "/products"
-                    )
+                # if stakeholder_response.status_code != 200:
+                #    return Response(
+                #        {
+                #            "data": stakeholder_response.json(),
+                #            "status": false,
+                #        },
+                #        status=status.http_400_BAD_REQUEST,
+                #    )
+                # else:
+                #    product_config_url = (
+                #        "https://api.razorpay.com/v2/accounts/" + razor_id + "/products"
+                #    )
 
-                    # Request Product Configuration
-                    product_config_data = {
-                        "product_name": serializer.validated_data.get(
-                            "product_name", "route"
-                        ),
-                        "tnc_accepted": serializer.validated_data.get("tnc_accepted"),
-                    }
+                #    # Request product configuration
+                #    product_config_data = {
+                #        "product_name": serializer.validated_data.get(
+                #            "product_name", "route"
+                #        ),
+                #        "tnc_accepted": serializer.validated_data.get("tnc_accepted"),
+                #    }
 
-                    product_config_response = requests.post(
-                        product_config_url,
-                        auth=(settings.RAZOR_KEY_ID, settings.RAZOR_KEY_SECRET),
-                        headers={"Content-Type": "application/json"},
-                        json=product_config_data,
-                    )
+                #    product_config_response = requests.post(
+                #        product_config_url,
+                #        auth=(settings.razor_KEY_ID, settings.RAZOR_KEY_SECRET),
+                #        headers={"content-type": "application/json"},
+                #        json=product_config_data,
+                #    )
 
-                    if product_config_response.status_code != 200:
-                        return Response(
-                            {
-                                "data": product_config_response.json(),
-                                "status": False,
-                            },
-                            status=status.HTTP_400_BAD_REQUEST,
-                        )
-                    else:
-                        product_id = json.loads(
-                            product_config_response.content.decode("utf-8")
-                        )["id"]
-                        serializer.validated_data["product_id"] = product_id
-                        serializer.save()
+                #    if product_config_response.status_code != 200:
+                #        return response(
+                #            {
+                #                "data": product_config_response.json(),
+                #                "status": false,
+                #            },
+                #            status=status.htTP_400_BAD_REQUEST,
+                #        )
+                #    else:
+                #        product_id = json.loads(
+                #            product_config_response.content.decode("utf-8")
+                #        )["id"]
+                #        serializer.validated_data["product_id"] = product_id
+                #        serializer.save()
 
-                        update_product_config_url = (
-                            "https://api.razorpay.com/v2/accounts/"
-                            + razor_id
-                            + "/products/"
-                            + product_id
-                            + "/"
-                        )
+                #        update_product_config_url = (
+                #            "https://api.razorpay.com/v2/accounts/"
+                #            + razor_id
+                #            + "/products/"
+                #            + product_id
+                #            + "/"
+                #        )
 
-                        # Request Update Product Configuration
-                        update_product_config_data = {
-                            "settlements": {
-                                "account_number": serializer.validated_data.get(
-                                    "account_number"
-                                ),
-                                "ifsc_code": serializer.validated_data.get("ifsc_code"),
-                                "beneficiary_name": serializer.validated_data.get(
-                                    "legal_business_name"
-                                ),
-                            },
-                            "tnc_accepted": serializer.validated_data.get(
-                                "tnc_accepted"
-                            ),
-                        }
+                # Request update product Configuration
+                #        update_product_config_data = {
+                #            "settlements": {
+                #                "account_number": serializer.validated_data.get(
+                #                    "account_number"
+                #                ),
+                #                "ifsc_code": serializer.validated_data.get("ifsc_code"),
+                #                "beneficiary_name": serializer.validated_data.get(
+                #                    "legal_business_name"
+                #                ),
+                #            },
+                #            "tnc_accepted": serializer.validated_data.get(
+                #                "tnc_accepted"
+                #            ),
+                #        }
 
-                        update_product_config_response = requests.patch(
-                            update_product_config_url,
-                            auth=(settings.RAZOR_KEY_ID, settings.RAZOR_KEY_SECRET),
-                            headers={"Content-Type": "application/json"},
-                            json=update_product_config_data,
-                        )
+                #        update_product_config_response = requests.patch(
+                #            update_product_config_url,
+                #            auth=(settings.rAZOR_KEY_ID, settings.RAZOR_KEY_SECRET),
+                #            headers={"content-Type": "application/json"},
+                #            json=update_product_config_data,
+                #        )
 
-                        if update_product_config_response.status_code != 200:
-                            return Response(
-                                {
-                                    "data": update_product_config_response.json(),
-                                    "status": False,
-                                },
-                                status=status.HTTP_400_BAD_REQUEST,
-                            )
-                        else:
-                            return Response(
-                                {
-                                    "data": {
-                                        "database_data": serializer.data,
-                                        "razorpay_data": [
-                                            account_response.json(),
-                                            stakeholder_response.json(),
-                                            product_config_response.json(),
-                                            update_product_config_response.json(),
-                                        ],
-                                    },
-                                    "status": True,
-                                },
-                                status=status.HTTP_201_CREATED,
-                            )
+                #        if update_product_config_response.status_code != 200:
+                #            return response(
+                #                {
+                #                    "data": update_product_config_response.json(),
+                #                    "status": False,
+                #                },
+                #                status=status.HTTP_400_BAD_REQUEST,
+                #            )
+                #        else:
+                #            return response(
+                #                {
+                #                    "data": {
+                #                        "database_data": serializer.data,
+                #                        "razorpay_data": [
+                #                            account_response.json(),
+                #                            stakeholder_response.json(),
+                #                            product_config_response.json(),
+                #                            update_product_config_response.json(),
+                #                        ],
+                #                    },
+                #                    "status": True,
+                #                },
+                #                status=status.HTTP_201_CREATED,
+                #            )
         # pay_NHi3IhG1fg4qhK
         return Response(
             {
